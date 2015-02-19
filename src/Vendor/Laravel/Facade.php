632fd560c1f04ef523b7d39b05edbe7d9797145d
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-02-19 21:12:08 -0200

    Remove license from php files

--- src/Vendor/Laravel/Facade.php
+++ src/Vendor/Laravel/Facade.php
@@ -1,23 +1,5 @@
 <?php
 
-/**
- * Part of the Google2FA package.
- *
- * NOTICE OF LICENSE
- *
- * Licensed under the 3-clause BSD License.
- *
- * This source file is subject to the 3-clause BSD License that is
- * bundled with this package in the LICENSE file.  It is also available at
- * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
- *
- * @package    Google2FA
- * @author     Antonio Carlos Ribeiro @ PragmaRX
- * @license    BSD License (3-clause)
- * @copyright  (c) 2013, PragmaRX
- * @link       http://pragmarx.com
- */
-
 namespace PragmaRX\Google2FA\Vendor\Laravel;
 
 use Illuminate\Support\Facades\Facade as IlluminateFacade;
