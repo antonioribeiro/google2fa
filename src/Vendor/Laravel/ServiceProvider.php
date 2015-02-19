704f23ba38d89906750c02349f2a7cfd5cebf3cf
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-02-19 21:34:02 -0200

    Fix tabs

--- src/Vendor/Laravel/ServiceProvider.php
+++ src/Vendor/Laravel/ServiceProvider.php
@@ -7,30 +7,30 @@ use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
 class ServiceProvider extends IlluminateServiceProvider
 {
 
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = true;
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = true;
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-	    $this->app->bind(
-		    $contract = 'PragmaRX\Google2FA\Contracts\Google2FA',
-		    $concrete = 'PragmaRX\Google2FA\Google2FA'
-	    );
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app->bind(
+			$contract = 'PragmaRX\Google2FA\Contracts\Google2FA',
+			$concrete = 'PragmaRX\Google2FA\Google2FA'
+		);
 
-	    $this->app['google2fa'] = $this->app->share(function($app) use ($contract)
-        {
-		    return $app->make($contract);
-	    });
-    }
+		$this->app['google2fa'] = $this->app->share(function($app) use ($contract)
+		{
+			return $app->make($contract);
+		});
+	}
 
 	/**
 	 * Get the services provided by the provider.
