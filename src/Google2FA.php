57ed36649b388ad9906750d8e41c264787f022d8
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-06-22 13:17:37 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Google2FA.php
+++ src/Google2FA.php
@@ -30,7 +30,6 @@ namespace PragmaRX\Google2FA;
  * @author     Antonio Carlos Ribeiro @ PragmaRX
  **/
 
-use PragmaRX\Google2FA\Exceptions\InvalidCharactersException;
 use PragmaRX\Google2FA\Exceptions\SecretKeyTooShortException;
 use PragmaRX\Google2FA\Support\Base32;
 use PragmaRX\Google2FA\Support\QRCode;
